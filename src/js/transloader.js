119636cfa14b5a8dda7e98ac77396a229528d0e8
sonicmax <max.teh.lueser@gmail.com>
2014-10-02 11:41:13 +0100

    Clean up transloader code
    More specifically, clean up the mess I made while adding the chrome.notifications code.

--- src/js/transloader.js
+++ src/js/transloader.js
@@ -1,27 +1,22 @@
 // Code from Milan
 
 function imageTransloader(info) {
-	console.log(info);
 	// setup clipboard shiv
 	var background = chrome.extension.getBackgroundPage();
 	var ta = background.document.createElement("textarea");
 	ta.id = "clipboard";
 	background.document.body.appendChild(ta);
-
 	// get filename
 	var filename = info.srcUrl.substring(info.srcUrl.lastIndexOf('/') + 1);
-
 	// facebook id fix
 	var pattern = /fbcdn\-sphotos/;
 	if (pattern.test(info.srcUrl)) {
 		filename = "fb.jpg";
 	}
-
 	// make sure it's not empty
 	if (filename === "") {
 		filename = "something.jpg";
 	}
-
 	// fetch the image
 	var fileGet = new XMLHttpRequest();
 	fileGet.open("GET", info.srcUrl, true);
@@ -29,42 +24,33 @@ function imageTransloader(info) {
 	fileGet.onreadystatechange = function(oEvent) {
 		if (fileGet.readyState === 4) {
 			if (fileGet.status === 200) {
-
-				// get neccessary metadata
+				// get necessary metadata
 				var filesize = fileGet.getResponseHeader("Content-Length");
 				var mimetype = fileGet.getResponseHeader("Content-Type");
-
 				// build blob
 				var dataview = new DataView(fileGet.response);
 				var blob = new Blob([ dataview ]);
-
 				// check if gif && > 2MB
 				if (filesize > (1024 * 1024 * 2) && mimetype === "image/gif") {
 					// notify user, updated for chrome.notifications api
 					chrome.notifications.create('fail', {
-						type : "basic",
-						title : "Image transloading failed",
-						message : "This gif is too big (>2MB)",
-						iconUrl : "src/images/lueshi_48_i.png"
-					}, function() {
+							type : "basic",
+							title : "Image transloading failed",
+							message : "This gif is too big (>2MB)",
+							iconUrl : "src/images/lueshi_48_i.png"
+					}, function(id) {
+						setTimeout(function() { clearNotification(id); }, 3000);
 					});
-					setTimeout(function() {
-						// removed second ; from end of next line
-						chrome.notifications.clear('fail', function() {
-						});
-					}, 3000);
 				} else {
 					// construct FormData object
 					var formData = new FormData();
 					formData.append("file", blob, filename);
-
 					// open connection to ETI and set callback
 					var xhr = new XMLHttpRequest();
 					xhr.open("POST", "http://u.endoftheinter.net/u.php", true);
 					xhr.onreadystatechange = function(xEvent) {
 						if (xhr.readyState === 4) {
 							if (xhr.status === 200) {
-
 								// parse response
 								var html = document.createElement('html');
 								html.innerHTML = xhr.responseText;
@@ -77,32 +63,21 @@ function imageTransloader(info) {
 											html.innerHTML);
 									return;
 								}
-
 								// send img code to clipboard
 								var clipboard = document
 										.getElementById('clipboard');
 								clipboard.value = value;
 								clipboard.select();
 								document.execCommand("copy");
-
-								// notify user, updated for chrome.notifications
-								// api
-								chrome.notifications
-										.create(
-												'succeed',
-												{
-													type : "basic",
-													title : "Image transloaded",
-													message : "The img code is now in your clipboard",
-													iconUrl : "src/images/lueshi_48.png"
-												}, function() {
-												});
-								setTimeout(function() {
-									// removed second ; from end of next line
-									chrome.notifications.clear('succeed',
-											function() {
-											});
-								}, 3000);
+								// notify user
+								chrome.notifications.create('succeed', {
+										type: "basic",
+										title: "Image transloaded",
+										message: "The img code is now in your clipboard",
+										iconUrl: "src/images/lueshi_48.png"
+								}, function(id) {
+									setTimeout(function() { clearNotification(id); }, 3000);
+								});
 							} else {
 								console.log("Error ", xhr.statusText);
 							}
@@ -117,4 +92,12 @@ function imageTransloader(info) {
 		}
 	};
 	fileGet.send(null);
-};
+}
+
+function clearNotification(id) {
+		chrome.notifications.clear(id,
+				function() {
+					// empty callback
+				}
+		);
+}
