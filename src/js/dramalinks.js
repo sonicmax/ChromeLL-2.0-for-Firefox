4461b09f89a520b11c9f442327d89412253181c4
sonicmax <max.teh.lueser@gmail.com>
2014-10-25 18:52:02 +0100

    Fix dramalinks bugs
    Properly handles background script response if ticker has already been inserted.
    Also fixes problems with the config.hide_dramalinks_topiclist setting

--- src/js/dramalinks.js
+++ src/js/dramalinks.js
@@ -1,21 +1,23 @@
 var config = {};
-var dramas;
+var dramalinks;
 var dramalinksFunctions = {
 	insertTicker : function(ticker) {
-		var h1 = document.getElementsByTagName('h1')[0];
-		if (config.dramalinks_below_topic 
-				&& document.getElementsByTagName('h2')[0]) {
+		var h1;
+		if (document.getElementsByTagName('h2')[0]) {
 			h1 = document.getElementsByTagName('h2')[0];
+		}
+		else {
+			h1 = document.getElementsByTagName('h1')[0];
 		}
 		h1.parentNode.insertBefore(ticker, h1.nextSibling);
 		if (config.hide_dramalinks) {
 			dramalinksFunctions.hideDrama();
 		}
-		if (dramas) {
-			ticker.innerHTML = dramas;
+		if (dramalinks) {
+			ticker.innerHTML = dramalinks;
 		}
 		else {
-			// wait for update_drama action
+			// wait for update_drama message from bg script
 		}
 	},
 	hideDrama : function() {
@@ -43,33 +45,33 @@ var dramalinksFunctions = {
 			need: "dramalinks"
 		}, function(response) {
 			var dramalinks_ticker = document.getElementById('dramalinks_ticker');
-			dramas = response.data;	
+			dramalinks = response.data;	
 			if (dramalinks_ticker) {
-				dramalinks_ticker.innerHTML = dramas;
-				if (dramas == '<a id="retry" href="javascript:void(0)">Error loading Dramalinks. Click to retry...</a>') {
+				dramalinks_ticker.innerHTML = dramalinks;
+				if (dramalinks == '<a id="retry" href="javascript:void(0)">Error loading Dramalinks. Click to retry...</a>') {
 					var retry = document.getElementById('retry');
 					retry.addEventListener('click', dramalinksFunctions.updateDramaTicker);
 				}
 			}
 			else {
-				console.log('problem with updateDramaTicker');
-				console.log(document.readyState);
+				console.log('error in updateDramaTicker');
+				console.log(document.readyState);			
 			}
 		});
 	},
 	addListener : function() {
 		chrome.runtime.onMessage.addListener(function(msg, sender) {
 			if (msg.action == 'update_drama') {
-				if (config.hide_dramalinks_topiclist 
-						&& !window.location.href.match(/topics/i)) {
-					return;
-				}
-				else if (document.getElementById('dramalinks_ticker')) {
+				if (document.getElementById('dramalinks_ticker')) {
 					dramalinksFunctions.updateDramaTicker();
 				}
+				else if (document.readyState == 'loading') {
+					document.addEventListener('DOMContentLoaded', function() {
+						dramalinksFunctions.updateDramaTicker();
+					});
+				}
 				else {
-					// do nothing
-					console.log('problem with onMessage listener');
+					console.log('error in onMessage listener');
 					console.log(document.readyState);
 				}
 			}
@@ -81,17 +83,26 @@ var dramalinksFunctions = {
 		}, function(response) {
 			config = response.data;
 			if (config.hide_dramalinks_topiclist
-					&& !window.location.href.match(/topics/i)) {
+					&& !window.location.href.match(/topics|history/i)) {
 				return;
-			}
+			}			
 			// set up listener to update ticker
 			dramalinksFunctions.addListener();
-			// request dramalinks and handle response from bg script
+			// request dramalinks
 			chrome.runtime.sendMessage({
 				need : "dramalinks"
 			}, function(response) {
-				// bg script only responds if returning cached dramalinks
-				dramas = response.data;
+				// cache response from bg script
+				dramalinks = response.data;
+				if (dramalinks) {
+					var dramalinks_ticker = document.getElementById('dramalinks_ticker');
+					if (dramalinks_ticker) {
+						dramalinks_ticker.innerHTML = dramalinks;
+					}
+					else {
+						// wait for insertTicker to handle cached response
+					}
+				}
 			});
 			// create ticker element
 			ticker = document.createElement("center");
