f633c7703ca2b217dead1d2dc7f1461b90641a6a
sonicmax <max.teh.lueser@gmail.com>
2014-10-28 16:24:20 +0000

    Make sure that wiki.js removes correct element
    Also changed run_at to "document_end" to make it faster.

--- src/js/wiki.js
+++ src/js/wiki.js
@@ -1,3 +1,12 @@
-if(document.getElementsByTagName("style")[1]){
-  document.getElementsByTagName("style")[1].remove();
+var styles = document.getElementsByTagName('style');
+if (styles) {
+	var style;
+	// iterate backwards as we are removing nodes
+	for (var i = styles.length; i--;) {
+		style = styles[i];
+		if (style.innerHTML == '@import "/skins/monobook/KHTMLFixes.css";') {
+			// remove this element as it breaks the search bar
+			style.remove();
+		}
+	}
 }
