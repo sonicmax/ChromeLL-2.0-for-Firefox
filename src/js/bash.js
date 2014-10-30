6faf653b7331e4fefc0a979fd8aa0ff18892ee61
sonicmax <max.teh.lueser@gmail.com>
2014-10-30 21:57:21 +0000

    Refactor addListeners function & improve accuracy
    I reused some of the code from messageList.js & moved the main processing
    outside of the onMessage listener. The function now ignores sigs - it will only
    submit quotes if they exist within the message content. I'm still a bit unsure of
    the while loop, but it hasn't caused any problems yet.

--- src/js/bash.js
+++ src/js/bash.js
@@ -2,8 +2,7 @@ var bash_functions = {
 	addListeners : function() {
 		// cache target element of contextmenu event in case
 		// user selects ETI Bash function
-		var bash_data = [];
-		var top, container, quotes, quote, quote_text, selection_text, container_text;
+		var top, container, quotes, selection_text;
 		document.addEventListener('contextmenu', function(evt) {
 			// cache relevant DOM elements in case they are needed by background script
 			selection_text = window.getSelection().toString();
@@ -16,40 +15,59 @@ var bash_functions = {
 		});
 		chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 			if (msg.action == 'context_element') {
-				if (quotes.length > 0) {
-					container_text = container.getElementsByClassName('message')[0].innerText;
-					// find original poster of selection_text
-					for (var i = 0, len = quotes.length; i < len; i++) {
-						quote = quotes[i];
-						if (quote.getElementsByClassName('quoted-message').length > 0) {
-							// contains nested quote
-							quote_text = quote.innerText.replace(quotes[i + 1].innerText, '');
-							container_text = container_text.replace(quote.innerText, '');
-							if (quote_text.indexOf(selection_text) > -1) {
-								bash_data[0] = bash_functions.getUsername(quote.getElementsByClassName('message-top')[0]);
-							}
-						}
-						else {
-							quote_text = quote.innerText;
-							container_text = container_text.replace(quote_text, '');
-							if (quote_text.indexOf(selection_text) > -1) {
-								bash_data[0] = bash_functions.getUsername(quote.getElementsByClassName('message-top')[0]);
-							}
-						}
-					}
-					// check container for selection_text
-					if (container_text.indexOf(selection_text) > -1) {
-						bash_data[0] = bash_functions.getUsername(top);
-					}
-				}
-				else {
-					bash_data[0] = bash_functions.getUsername(top);
-				}
-				bash_data[1] = bash_functions.getURL(top);		
-				// respond with username and url
+				// check that selection_text exists in message/quoted message
+				var bash_data = bash_functions.createArray(quotes, container, top, selection_text);
+				// respond with username and url (or null value if no match was found)
 				sendResponse({"data": bash_data});
 			}
 		});	
+	},
+	createArray : function(quotes, container, top, selection_text) {
+		var bash_data = [];
+		var quote, tops, message;
+		if (quotes.length > 0) {
+			for (var i = 0, len = quotes.length; i < len; i++) {
+				quote = quotes[i];
+				tops = quote.getElementsByClassName('message-top');
+				message = bash_functions.getMessage(quote, quotes, tops, i);
+				if (message.indexOf(selection_text) > -1) {
+					// get username from message-top of quoted-message
+					bash_data[0] = bash_functions.getUsername(quote.getElementsByClassName('message-top')[0]);
+					bash_data[1] = bash_functions.getURL(top);
+					return bash_data;
+				}
+				else {
+					bash_data[0] = null;	
+				}						
+			}
+			// get outermost post last
+			quote = container.getElementsByClassName('message')[0];
+			// pass -1 to make sure that all quoted posts are removed
+			message = bash_functions.getMessage(quote, quotes, tops, -1);
+			if (message.indexOf(selection_text) > -1) {
+				// get username from message-top of message-container
+				bash_data[0] = bash_functions.getUsername(top);
+				bash_data[1] = bash_functions.getURL(top);				
+				return bash_data;
+			}
+			else {
+				bash_data[0] = null;							
+			}
+		}
+		else if (quotes.length === 0) {
+			// get username and post content belonging to first message-top
+			message = bash_functions.getMessage(container);
+			if (message.indexOf(selection_text) > -1) {
+				bash_data[0] = bash_functions.getUsername(top);	
+				bash_data[1] = bash_functions.getURL(top);				
+				return bash_data;
+			}
+			else {
+				bash_data[0] = null;							
+			}						
+		}
+		// no matches
+		return bash_data;
 	},
 	getUsername : function(top) {
 		// returns username from message-top
@@ -74,6 +92,35 @@ var bash_functions = {
 			}
 		}
 	},
+	getMessage : function(quote, quotes, tops, index) {
+		// takes message/quoted-message HTML and returns text
+		var sig;
+		if (!quotes) {
+			var message = quote.innerText;
+			sig = message.lastIndexOf('---');
+			if (sig > -1) {
+				message = message.substring(0, sig);
+			}
+		}
+		else {
+			var message = quote.innerText;
+			var top, other_quote;
+			for (var i = 0, len = tops.length; i < len; i++) {
+				top = tops[i];
+				message = message.replace(top.innerText, '');
+				if (i !== index) {
+					other_quote = quotes[i].innerText.replace(top.innerText, '');
+					message = message.replace(other_quote, '');
+				}
+			}
+			sig = message.lastIndexOf('---');
+			if (sig > -1) {
+				message = message.substring(0, sig);
+			}		
+		}
+		message = message.replace('[quoted text omitted]', '');
+		return message.trim();
+	},	
 	init: function() {
 		chrome.runtime.sendMessage({
 			need : "config"
