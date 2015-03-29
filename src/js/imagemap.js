25c82772f25620b015251ded477a4b86202c11ae
sonicmax <max.teh.lueser@gmail.com>
2015-03-29 21:16:50 +0100

    Add comments for clarity
    Also made a minor change to the caching process (removed dataToCache object
    as we can pass the image data directly to updateCache method).

--- src/js/imagemap.js
+++ src/js/imagemap.js
@@ -1,16 +1,16 @@
 var imagemap = {
 	cache: {},
-	dataToCache: {},
 	currentPage: 1,
 	lastPage: '?',			
 	init: function() {
+		// Called after user clicks Browse Imagemap button
 		var that = this;
 		this.loadCache(function(cached) {
 			that.cache = cached.imagemap;
 			that.getImagemap.call(that, that.processResponse);
 		});
 	},
-	getImagemap: function(callback) {
+	getImagemap: function(callback) {		
 		var page;
 		(this.currentPage === 1)
 				? page = ''
@@ -28,22 +28,25 @@ var imagemap = {
 		xhr.send();
 	},			
 	processResponse: function(imagemap) {
+		// Scrape grid of images from imagemap
 		var imageGrid = this.scrape(imagemap);
 		var infobar = imagemap.getElementsByClassName('infobar')[1];
-		var anchors = infobar.getElementsByTagName('a');					
+		var anchors = infobar.getElementsByTagName('a');
+		// Find size of imagemap by checking the page navigation anchor tags
 		this.lastPage = anchors[anchors.length - 1].innerHTML;
+		// Now ready to create imagemap popup and cache the thumbnails (if necessary)
 		this.createPopup.call(this, imageGrid);
 		this.sendToEncoder(imageGrid);
 	},
 	scrape: function(imagemap) {
+		// Returns modified grid of images from imagemap for popup
 		var imageGrid = imagemap.getElementsByClassName('image_grid')[0];
 		var imgs = imageGrid.getElementsByTagName('img');
-		
 		for (var i = 0, len = imgs.length; i < len; i++) {
 			var img = imgs[i];
 			var src = img.src;
+			// Replace src attribute value with cached base64 strings (if they exist)
 			if (this.cache[src]) {
-				// replace img src with cached base64 strings
 				img.setAttribute('oldsrc', img.src);
 				img.src = this.cache[src].data;							
 			}
@@ -61,13 +64,13 @@ var imagemap = {
 		return imageGrid;
 	},			
 	createPopup: function(imageGrid, searchResults) {
-		var that = this;
+		// Create div element for popup and style as required
+		var that = this;		
 		var div = document.createElement('div');
 		var width = window.innerWidth;
 		var height = window.innerHeight;
 		var bodyClass = document.getElementsByClassName('body')[0];
 		var anchorHeight;	
-		// TODO - move these to messageList.addCSSRules method
 		div.id = "map_div";
 		div.style.position = "fixed";				
 		div.style.width = (width * 0.95) + 'px';
@@ -93,12 +96,12 @@ var imagemap = {
 			header.style.width = '100%';
 			header.style.fontSize = '16px';
 			div.appendChild(header);				
-			// account for header's style properties
+			// Account for header's style properties
 			imageGrid.style.maxHeight = ((height * 0.95) / 2) - 51 + 'px';
 			imageGrid.style.maxWidth = (width * 0.95) - 21 + 'px';
 		}
 		else {
-			// subtract 6px to prevent scrollbar from overlapping rounded corners
+			// Subtract 6px to prevent scrollbar from overlapping rounded corners
 			imageGrid.style.maxWidth = (width * 0.95) - 6 + 'px';
 			imageGrid.style.maxHeight = ((height * 0.95) / 2)  - 6 + 'px';
 		}
@@ -115,25 +118,30 @@ var imagemap = {
 		}
 		document.body.appendChild(div);
 		document.body.style.overflow = 'hidden';
+		// Prevent scrolling in imagemap div from affecting the rest of the page
 		bodyClass.addEventListener('mousewheel', this.preventScroll);
+		// Add click listeners to close popup/copy img code to clipboard when appropriate
 		bodyClass.addEventListener('click', this.closePopup.bind(this));
 		div.addEventListener('click', function(evt) {
 			that.clickHandler.call(that, evt);
 			evt.preventDefault();
 		});
 		if (!searchResults) {
+			// Load new page after user has scrolled to bottom of existing page.
+			// Uses debouncing to improve performance.
 			imageGrid.addEventListener('scroll', this.debouncer.bind(this));
 		}
 	},
 	sendToEncoder: function(imageGrid) {
+		// Iterate over images and send to encoder method
 		var imgs = imageGrid.getElementsByTagName('img');
 		for (var i = 0, len = imgs.length; i < len; i++) {
 			var img = imgs[i];
 			var src = img.src;
 			// Images without oldsrc attribute need to be cached
 			if (!img.getAttribute('oldsrc')) {
-				
-				if (img.parentNode.className === 'img-loaded') {
+				// We need to look in different places for loaded and placeholder image hrefs
+				if (img.parentNode.className === 'img-loaded') {					
 					var href = img.parentNode.parentNode.href;
 				}
 				else {
@@ -145,17 +153,17 @@ var imagemap = {
 		}
 	},						
 	encodeToBase64: function(src, href) {
-		var that = this;
+		// Draw each image to canvas so we can encode it as base64 string
+		var that = this;		
 		var canvas = document.createElement('canvas');
 		var context = canvas.getContext('2d');
 		var img = new Image;
 		img.crossOrigin = "Anonymous";
 		img.onload = function() {
-			var dataURI;
 			canvas.height = img.height;
 			canvas.width = img.width;
 			context.drawImage(img, 0, 0);
-			dataURI = canvas.toDataURL();
+			var dataURI = canvas.toDataURL();
 			var imageData = {
 					'dataURI': dataURI, 
 					'src': src, 
@@ -163,16 +171,17 @@ var imagemap = {
 					'index': i
 			};
 			that.prepareCacheData.call(that, imageData);
-			// canvas = null;
 		};
+		// Use cors-anywhere server to deal with CORS restrictions
 		img.src = window.location.protocol + '//cors-for-chromell.herokuapp.com/' + src;
 	},
 	prepareCacheData: function(imageData) {
+		var cacheData = {};
 		var dataURI = imageData.dataURI;
 		var href = imageData.href;
 		var src = imageData.src;
 		var i = imageData.index;
-		// thumbnails are always jpgs - fullsize image could have a different file format (found in href)			
+		// Thumbnails are always jpgs - fullsize image could have a different file format (found in href)			
 		var extension = href.match(/\.(gif|jpg|png)$/i)[0];
 		var fullsize = src.replace('.jpg', extension);
 		fullsize = fullsize.replace('dealtwith.it/i/t', 'endoftheinter.net/i/n');
@@ -180,24 +189,25 @@ var imagemap = {
 		filename = decodeURIComponent(filename);
 		
 		if (!filename || !fullsize || !dataURI) {
+			// This probably shouldn't happen
 			console.log('Error while caching image: ', '\n', src, filename, fullsize, dataURI);
 			return;
 		}
 		else {		
-			this.dataToCache[src] = {"filename": filename, "fullsize": fullsize, "data": dataURI};
+			cacheData[src] = {"filename": filename, "fullsize": fullsize, "data": dataURI};
 			// Finished encoding image - update cache
-			this.updateCache();
+			this.updateCache(cacheData);
 		}
 	},
-	updateCache: function() {
+	updateCache: function(cacheData) {
 		if (Object.keys(this.cache).length === 0) {
-			// first time caching
-			this.cache = this.dataToCache;
+			// First time caching 
+			this.cache = cacheData;
 		}
 		else {
-			// add current page to existing data
-			for (var i in this.dataToCache) {
-				this.cache[i] = this.dataToCache[i];							
+			// Add new image to existing cached data
+			for (var i in cacheData) {
+				this.cache[i] = cacheData[i];							
 			}
 		}
 	},
@@ -205,7 +215,9 @@ var imagemap = {
 		chrome.storage.local.get("imagemap", function(cache) {
 			if (Object.keys(cache).length === 0) {
 				// Return empty imagemap object
-				callback( {"imagemap": {} });
+				callback({
+					"imagemap": {}
+				});
 			}
 			else if (cache) {
 				callback(cache);
@@ -214,11 +226,12 @@ var imagemap = {
 	},
 	saveCache: function() {
 		chrome.storage.local.set({"imagemap": this.cache}, function() {
+			// Clear cache object after storage.local.set method has completed
 			imagemap.cache = {};
-			imagemap.dataToCache = {};
 		});
 	},
 	debouncer: function() {
+		// Prevents scroll handler from being called repeatedly
 		clearTimeout(this.debounceTimer);
 		this.debounceTimer = setTimeout(this.scrollHandler.bind(this), 250);
 	},
@@ -226,15 +239,15 @@ var imagemap = {
 	scrollHandler: function(imageGrid) {
 		var that = this;
 		var imageGrid = document.getElementsByClassName('image_grid')[0]
-		// check whether user is at end of current page
-		// (minus 5 pixels from clientHeight to account for large zoom levels)
+		// Check whether user is at end of current page - subtract 5 pixels from clientHeight 
+		// to account for large zoom levels)
 		if (imageGrid.scrollTop >= imageGrid.scrollHeight - imageGrid.clientHeight - 5) {			
 			if (this.currentPage === this.lastPage) {
-				// no more pages to load
+				// No more pages to load
 				return;
 			}
 			else {
-				// load next page and append to current grid
+				// Load next page and append to current grid 
 				this.currentPage++;
 				this.getImagemap(function(imagemap) {
 					var newGrid = that.scrape(imagemap);
@@ -249,9 +262,9 @@ var imagemap = {
 			return;
 		}
 		else if (evt.target.tagName === 'IMG') {
-			// get img code & copy to clipboard via background page
 			var clipboard = {};	
 			if (evt.target.getAttribute('searchresult')) {
+				// Use oldsrc attribute as src attribute is data URI
 				var src = evt.target.getAttribute('oldsrc'); 
 			}
 			else {
@@ -261,35 +274,36 @@ var imagemap = {
 				else {
 					var src = evt.target.src;
 				}
-				var href = evt.target.parentNode.href;					
+				var href = evt.target.parentNode.href;
 				var regex = /\.(gif|jpg|png)$/i;
+				// Make sure that we're using the href extension, not the thumbnail extension
 				var extension = href.match(regex);
 				var extensionToReplace = src.match(regex);
-				// replace thumbnail file extension with file extension of fullsize image
 				src = src.replace(extensionToReplace[0], extension[0]);
 			}
-			// replaces thumbnail location with location of fullsize image
+			// Create LLML img code string
 			clipboard.quote =  '<img src="' + src.replace('dealtwith.it/i/t', 'endoftheinter.net/i/n') + '" />';
+			// Pass data to background page so we can copy it to clipboard
 			chrome.runtime.sendMessage(clipboard);
 		}
-		// always close popup after click, even if user didn't click on an image
+		// Always close popup after click event, even if user didn't click on an image
 		this.closePopup.call(this);
 		document.removeEventListener('click', this.clickHandler);
 		evt.preventDefault();
 	},
 	closePopup: function() {
-		var div = document.getElementById('map_div');
-		if (!div) {
-			div = document.getElementById('search_results');				
-		}
+		// Remove popup div, style changes and event listeners
+		var div = document.getElementById('map_div') || document.getElementById('search_results');
 		var bodyClass = document.getElementsByClassName('body')[0];
 		if (div) {
 			document.body.removeChild(div);
 		}
 		bodyClass.style.opacity = 1;
 		document.body.style.overflow = 'initial';
-		bodyClass.removeEventListener('mousewheel', this.preventScroll);
+		bodyClass.removeEventListener('mousewheel', this.preventScroll);		
 		this.currentPage = 1;
+		// Save cache after closing popup - this prevents us from having to continually access the
+		// chrome.storage APIs after encoding each image (which would be costly).
 		this.saveCache.call(this);
 	},
 	preventScroll: function(evt) {
@@ -299,7 +313,7 @@ var imagemap = {
 		init: function() {
 			var that = this;
 			var query = document.getElementById('image_search').value;
-			// make sure that query isnt empty
+			// Check that query contains characters other than whitespace
 			if (/\S/.test(query)) {
 				this.lookup(query, function(results, query) {
 					if (!document.getElementById('search_results')) {
@@ -309,20 +323,20 @@ var imagemap = {
 						var oldGrid = document.getElementById('results_grid') || document.getElementById('no_results_grid');								
 						oldGrid.remove();
 						// display loading_image element while waiting for results div to update
-						document.getElementById('loading_image').style.display = 'block';
-						
-					}							
-					that.prepareResults(results, query);
+						document.getElementById('loading_image').style.display = 'block';				
+					}
+					that.getMatches(results, query);
 				});
 			}
 			else {
-				// detected empty search box after keyup event - close imagemap popup (if it exists)
+				// Empty search box after keyup event - close imagemap popup (if it exists)
 				if (document.getElementById('search_results')) {
 					imagemap.closePopup();
 				}
 			}
 		},
 		lookup: function(query, callback) {
+			// Iterate over imagemap, check for query and push matches to new array
 			var results = [];
 			imagemap.loadCache(function(cached) {
 				for (var i in cached.imagemap) {
@@ -334,13 +348,15 @@ var imagemap = {
 				callback(results, query);
 			});				
 		},
-		prepareResults: function(results, query) {
+		getMatches: function(results, query) {
 			var that = this;
 			var resultsToShow = results;
 			if (results.length === 0) {
-				this.formatResults(false, query);
+				// No matches - pass false value instead of data
+				this.updatePopup(false, query);
 			}
 			else {
+				// Get image data for each match from cache
 				imagemap.loadCache(function(cached) {
 					var data = {};
 					for (var i = 0, len = results.length; i < len; i++) {
@@ -351,29 +367,25 @@ var imagemap = {
 				});
 			}
 		},
-		formatResults: function(data, query) {	
-			if (!data) {
-				this.updatePopup(false, query);
+		formatResults: function(data, query) {
+			// Format search results to be displayed in popup
+			var grid = document.createElement('div');	
+			grid.className = 'image_grid';
+			grid.id = 'results_grid';
+			grid.style.clear = 'left';		
+			for (var i in data) {
+				var block = document.createElement('div');
+				block.className = 'grid_block';
+				var img = document.createElement('img');
+				img.setAttribute('oldsrc', data[i].fullsize);
+				img.setAttribute('searchresult', true);
+				img.src = data[i].data;
+				block.className = 'grid_block';
+				block.style.display = 'inline';
+				block.appendChild(img);
+				grid.appendChild(block);						
 			}
-			else {
-				var grid = document.createElement('div');	
-				grid.className = 'image_grid';
-				grid.id = 'results_grid';
-				grid.style.clear = 'left';		
-				for (var i in data) {
-					var block = document.createElement('div');
-					block.className = 'grid_block';
-					var img = document.createElement('img');
-					img.setAttribute('oldsrc', data[i].fullsize);
-					img.setAttribute('searchresult', true);
-					img.src = data[i].data;
-					block.className = 'grid_block';
-					block.style.display = 'inline';
-					block.appendChild(img);
-					grid.appendChild(block);						
-				}
-				this.updatePopup(grid, query);
-			}
+			this.updatePopup(grid, query);
 		},
 		createPopup: function(query) {
 			var header = document.createElement('div');
