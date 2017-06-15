e1979d3fb5c5b79098b9bac6cac5771e055b18b7
max <max.teh.lueser@gmail.com>
2017-06-15 12:10:58 +0100

    Account for inbox threads when finding topic creator

--- src/js/messageList.js
+++ src/js/messageList.js
@@ -3145,7 +3145,12 @@ var messageList = {
 				}
 				
 				var tops = document.getElementsByClassName('message-top');									
-				var tagsHtml = document.getElementsByTagName('h2')[0].innerHTML;
+				var tagsHtml = "";
+								
+				// We don't need to check PM threads for tags
+				if (window.location.href.pathname !== "/inboxthread.php") {
+					tagsHtml = document.getElementsByTagName('h2')[0].innerHTML;
+				}
 				
 				if (tagsHtml.indexOf(">Anonymous</a>") > -1) {
 					creator = "human #1";
