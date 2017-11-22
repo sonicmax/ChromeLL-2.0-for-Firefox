ca37197c1c5499bd535a48e621f39cb5efff4029
max <max.teh.lueser@gmail.com>
2017-11-22 13:30:20 +0000

    Tidying up some code

--- src/js/search.js
+++ src/js/search.js
@@ -1,15 +1,16 @@
-var search = {
-	sangreen : function() {
-		document
-				.addEventListener(
-						'keyup',
-						function(evt) {
-							if (evt.target.value
-									&& evt.target.value.toLowerCase() === 'sangreen') {
-								document.body.style.backgroundImage = 'url("http://i.imgur.com/SVCxE.gif")';
-							}
-						});
-	}
-}
-
-search.sangreen();
+(() => {
+	
+	var oldImage;
+	
+	// sangreen my brothers
+	document.addEventListener('keyup', evt => {
+		if (evt.target.value && evt.target.value.toLowerCase() === 'sangreen') {
+			oldImage = document.body.style.backgroundImage;
+			document.body.style.backgroundImage = 'url("http://i.imgur.com/SVCxE.gif")';
+		}
+		else {
+			document.body.style.backgroundImage = oldImage;
+		}
+	});
+	
+})();
